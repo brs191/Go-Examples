f0e766f1d85d12452fa6f71d3bfaf5a52960303a
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2021-02-08 14:23:45 +0900

    instruction to run prometheus

--- basicPrometheus/main.go
+++ basicPrometheus/main.go
@@ -2,6 +2,20 @@ package main
 
 import "basicPrometheus/prometheus"
 
+/*
+1. Install Prometheus server in the local system
+https://computingforgeeks.com/install-prometheus-server-on-debian-ubuntu-linux/
+
+2. Create a server/service to listen at a port ex: 2112;; This is a prometheus target/endpoint
+
+3. Create a Scrape configuration for the new server/service with the port ex: 2112
+
+4. Install Grafana as https://prometheus.io/docs/visualization/grafana/
+
+5. Link prometheus datastore @default prometheus 9090
+
+6. Create a dashboard and look for the metrics in either default prometheus or our endpoint/service
+ */
 func main() {
 	prometheus.Init()
 	println("basicPrometheus done!!")
